7d171ad699d636e93426ba9b4021ed49113ef334
akadashiso <akadashiso@gmail.com>
2021-02-02 13:35:46 +0000

    Added favorite function

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
   attachment :profile_image, destroy: false
   validates :introduction,  length: {maximum: 50}
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
+  has_many :books
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 end
